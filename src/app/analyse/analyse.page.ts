619b03455100cc9c72ea40203284d898f438c4c3
huggos4 <kaspor16@hotmail.fr>
2019-11-15 15:30:59 +0100

    borderaux

--- src/app/analyse/analyse.page.ts
+++ src/app/analyse/analyse.page.ts
@@ -8,9 +8,17 @@ import * as HighCharts from 'highcharts';
 })
 export class analysePage {
 
-  public date: string = new Date().toString();
+  public date: Date = null;
+  public sdate: string = null;
+  public boAnnee: boolean = false;
+  public boBilan: boolean = false;
+  public boMois: boolean = true;
 
-  constructor() {}
+
+  constructor() {
+    this.date = new Date();
+    this.getDate(this.date.getMonth().toString());
+  }
 
   ionViewWillEnter(){
     let myChart = HighCharts.chart('container', {
@@ -65,4 +73,119 @@ export class analysePage {
     });
   }
 
+  clickAnnee(){
+    this.boAnnee = true;
+    this.boBilan = false;
+    this.boMois = false;
+
+    this.date = new Date();
+    this.sdate = this.date.getFullYear().toString();
+  }
+
+  clickMois(){    
+    this.boAnnee = false;
+    this.boBilan = false;
+    this.boMois = true;
+
+    this.date = new Date();
+    this.getDate(this.date.getMonth().toString());
+  }
+
+  clickBilan(){
+    this.boAnnee = false;
+    this.boBilan = true;
+    this.boMois = false;
+    this.sdate = localStorage.getItem("dBilan");
+  }
+  
+add(){
+  let d = new Date();
+  let year = d.getFullYear();
+  let month = d.getMonth();
+  let day = d.getDate();
+  if(this.boAnnee){
+    this.date = new Date(this.date.getFullYear() + 1, this.date.getMonth() , this.date.getDay())
+    this.sdate = this.date.getFullYear().toString();
+  }
+  if(this.boMois){
+    this.date = new Date(this.date.getFullYear(), this.date.getMonth() + 1, this.date.getDay());
+    this.getDate(this.date.getMonth().toString());
+  }
+}  
+
+less(){
+  if(this.boAnnee){
+    this.date = new Date(this.date.getFullYear() - 1, this.date.getMonth(), this.date.getDay());
+    this.sdate = this.date.getFullYear().toString();
+  }
+  if(this.boMois){
+    this.date = new Date(this.date.getFullYear(), this.date.getMonth() - 1, this.date.getDay());
+    this.getDate(this.date.getMonth().toString());
+  }
+}
+
+
+  getDate(mois: string){
+    switch(mois){
+      case '12': {
+        this.sdate = "Décembre";
+        break;
+
+      }
+      case '11': {
+        this.sdate = "Novembre";
+        break;
+        
+      }
+      case '10': {
+        this.sdate = "Octobre";
+        break;
+        
+      }
+      case '9': {
+        this.sdate = "Septembre";
+        break;
+        
+      }
+      case '8': {
+        this.sdate = "Aout";
+        break;
+        
+      }
+      case'7': {
+        this.sdate = "Juillet";
+        break;
+        
+      }
+      case '6': {
+        this.sdate = "Juin";
+        break;
+        
+      }
+      case '5': {
+        this.sdate = "Mai";
+        break;
+        
+      }
+      case '4': {
+        this.sdate = "Avril";
+        break;
+        
+      }
+      case '3': {
+        this.sdate = "Mars";
+        break;
+        
+      }
+      case '2': {
+        this.sdate = "Février";
+        break;
+        
+      }
+      case '1': {
+        this.sdate = "Janvier";
+        break;
+      }
+    }
+  }
 }
